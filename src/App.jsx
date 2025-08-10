a9e1a205145fbd48f91f2c4dea99e19abaf2e58d
parksujin <sherry5667@ewha.ac.kr>
2025-08-10 19:14:08 +0900

    router

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import "./App.css";
 import React from "react";
 import "antd/dist/reset.css";
 import Login from "./pages/login/Login.jsx";
+import Home from "./pages/Home.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
 	return (
-		<div>
-			<Login />
-		</div>
+		<Router>
+			<Routes>
+				<Route path="/home" element={<Home />} />
+				<Route path="/Login" element={<Login />} />
+			</Routes>
+		</Router>
 	);
 }
 
