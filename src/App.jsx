bcdee415555abc8e5bfff88829e1385a51a111fe
ttt-1-2 <tranthutrangcppp@gmail.com>
2025-08-20 17:20:45 +0900

    배포 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import "antd/dist/reset.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import StudyListPage from "./pages/study/StudyListPage";
 import StudyWritePage from "./pages/study/StudyWritePage";
 import StudyDetailPage from "./pages/study/StudyDetailPage";
