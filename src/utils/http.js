09ba7bdcc66e51c73651edbe8f7ba0e7ad1ef46e
parksujin <sherry5667@ewha.ac.kr>
2025-08-22 03:20:49 +0900

    netlify error

--- src/utils/http.js
+++ src/utils/http.js
@@ -4,7 +4,7 @@ const http = axios.create({
 	//#Mixed Content (https에서 http 요청)에러가 떠서 실제 서버 baseURL은 netlify.toml파일에 설정했습니다.
 	baseURL: "/api",
 	headers: { "Content-Type": "application/json" },
-	// withCredentials: true,
+	withCredentials: true,
 });
 
 export const setAuthToken = (token) => {
