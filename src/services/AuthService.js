aaad01f77217c966f61e875da5ccc833add9e8b7
parksujin <sherry5667@ewha.ac.kr>
2025-08-22 10:46:28 +0900

    login구현현

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -8,3 +8,13 @@ export const loginUser = async ({ email, password }) => {
 	});
 	return res.data;
 };
+
+//2. 회원가입
+export const registerUser = async ({ nickname, email, password }) => {
+	const res = await http.post("/auth/signup", {
+		nickname,
+		email,
+		password,
+	});
+	return res.data;
+};
