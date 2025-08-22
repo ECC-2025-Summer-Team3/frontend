130a6e1bf81b9ef0bf77285c3f86fb20f96c53e1
ttt-1-2 <tranthutrangcppp@gmail.com>
2025-08-22 12:21:52 +0900

    feat: http.js 수정

--- src/utils/http.js
+++ src/utils/http.js
@@ -4,7 +4,7 @@ const http = axios.create({
 	//#Mixed Content (https에서 http 요청)에러가 떠서 실제 서버 baseURL은 netlify.toml파일에 설정했습니다.
 	baseURL: "/api",
 	headers: { "Content-Type": "application/json" },
-	withCredentials: true,
+	withCredentials: false,
 });
 
 export const setAuthToken = (token) => {
