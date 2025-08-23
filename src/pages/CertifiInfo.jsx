06669620dbc68f92af84d8fb622cdad51490b4a0
parksujin <sherry5667@ewha.ac.kr>
2025-08-24 01:29:16 +0900

    자격증상세정보

--- src/pages/CertifiInfo.jsx
+++ src/pages/CertifiInfo.jsx
@@ -5,6 +5,9 @@ import {
 	Blank,
 	InfoText,
 	InfoContent,
+	OfficialLink,
+	BlankStar,
+	TitleWrapper,
 } from "../styles/CertifiInfoStyle";
 import { useParams } from "react-router-dom";
 import { fetchCertificateDetail } from "../services/CertificateService";
@@ -28,10 +31,40 @@ function CertifiInfo() {
 
 	return (
 		<PageWrapper>
-			<InfoTitle>{certificate?.certificateName}</InfoTitle>
+			<TitleWrapper>
+				<InfoTitle>{certificate?.certificateName}</InfoTitle>
+				<BlankStar />
+			</TitleWrapper>
 			<Blank />
 			<InfoText>시험개요</InfoText>
-			<InfoContent>{certificate.overview}</InfoContent>
+			<InfoContent>{certificate?.overview}</InfoContent>
+			<Blank />
+			<InfoText>시행기관</InfoText>
+			<InfoContent>{certificate?.organization}</InfoContent>
+			<Blank />
+			<InfoText>시험방식</InfoText>
+			<InfoContent>{certificate?.examMethod}</InfoContent>
+			<Blank />
+			<InfoText>합격기준</InfoText>
+			<InfoContent>{certificate?.passingCriteria}</InfoContent>
+			<Blank />
+			<InfoText>시험자격</InfoText>
+			<InfoContent>{certificate?.qualification}</InfoContent>
+			<Blank />
+			<InfoText>시험료</InfoText>
+			<InfoContent>{certificate?.fee}</InfoContent>
+			<Blank />
+			<InfoText>특이사항</InfoText>
+			<InfoContent>{certificate?.features}</InfoContent>
+			<Blank />
+			<InfoText />
+			<OfficialLink
+				href={certificate?.officialSite}
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				공식사이트
+			</OfficialLink>
 		</PageWrapper>
 	);
 }
--- src/styles/CertifiInfoStyle.js
+++ src/styles/CertifiInfoStyle.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { StarOutlined } from "@ant-design/icons";
 
 export const PageWrapper = styled.div`
 	width: 100%;
@@ -13,6 +14,7 @@ export const InfoTitle = styled.h1`
 	border-radius: 10px;
 	font-size: clamp(1rem, 2.5vw, 1.5rem);
 	transition: font-size 0.2s ease;
+	margin: 0;
 `;
 
 export const Blank = styled.div`
@@ -34,4 +36,27 @@ export const InfoContent = styled.p`
 	padding-bottom: 15px;
 	text-align: left;
 	color: black;
+`;
+
+export const OfficialLink = styled.a`
+	font-size: clamp(0.5rem, 1.5vw, 1rem);
+	transition: font-size 0.2s ease;
+	display: block;
+	width: 100%;
+	text-align: left;
+	color: blue;
+	cursor: pointer;
+`;
+
+export const BlankStar = styled(StarOutlined)`
+	font-size: clamp(1.2rem, 4vw, 1.8rem);
+	color: rgb(255, 196, 0);
+	margin-left: 5px;
+`;
+
+export const TitleWrapper = styled.div`
+	display: flex;
+	align-items: center;
+	height: clamp(1.5rem, 4vw, 2rem);
+	justify-content: center;
 `;
